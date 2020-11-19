ff9db3430dd794d66ffefbf960a5d0d011aeed60
Kevin Zhou <kjzhou2@linux-02.ews.illinois.edu>
2020-11-18 18:18:46 -0600

    working on getting the spotify key to work

--- api_pkg_test/api_pkg_test.cpp
+++ api_pkg_test/api_pkg_test.cpp
@@ -1,9 +1,13 @@
 #include <curl/curl.h>
 #include <string>
 #include <iostream>
-
+#include "single_include/nlohmann/json.hpp"
+#include "keys.h"
+using json = nlohmann::json;
 using std::string;
 
+
+//to compile: clang++ api_pkg_test.cpp -std=c++11 -l curl
 string data;
 size_t writeCallback(char* buf, size_t size, size_t nmemb, void* up) {
     //callback must have this declaration
@@ -17,10 +21,19 @@ size_t writeCallback(char* buf, size_t size, size_t nmemb, void* up) {
     return size*nmemb; //tell curl how many bytes we handled
 }
 
+//access key: jg3OTQwNGYzNzViNGNhMDkyZGJmMDI2Yjg1YzhkN2Q6ZGQ4Njk4MjExYzQyNDhhNTgzOTMyM2ZiNzExNWNmNzkgDQo=
+//spotify:playlist:7DYJeAV6l6GwfXLQtLV5Rd
+
 int main(int argc, char** argv) {
     CURL *curl = curl_easy_init();
     if (curl) {
-        curl_easy_setopt(curl,  CURLOPT_URL, "https://github-dev.cs.illinois.edu/cs225-fa20/erf3-caleqw2-kjzhou2-evanmm3");
+
+        //we're going to access the Spotify API by using the Client Credentials Flow
+        //https://developer.spotify.com/documentation/general/guides/authorization-guide/#client-credentials-flow
+        //first curl the api/token endpoint with the header with a base64 encoded clientkey:clientsecret
+        //that will return a json object with a access token
+        //Then we can access any public endpoint by adding that access token in the header of the curl
+        curl_easy_setopt(curl,  CURLOPT_URL, "https://api.spotify.com/v1/playlists/spotify:playlist:7DYJeAV6l6GwfXLQtLV5Rd");
         curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
         curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
         std::string response_string;
