f5975c45a614938dc7106c44a6e9b905f542ce2a
Kevin Zhou <kjzhou2@illinois.edu>
2020-12-01 18:42:20 -0600

    fixed the spotify logic to stop backtracing, needs to fix character encoding for output to csv

--- SpotifyPython/SpotifyAPI/SpotifyAPI.py
+++ SpotifyPython/SpotifyAPI/SpotifyAPI.py
@@ -1,12 +1,18 @@
+import csv
+
 import spotipy
 import keys
 import pprint
 import queue
+from itertools import combinations
 from spotipy.oauth2 import SpotifyClientCredentials
 
 pp = pprint.PrettyPrinter(indent=4)
-auth_manager = SpotifyClientCredentials(keys.CLIENT_ID, keys.CLIENT_SECRET) #is in the .gitignore, ask kevin for details or take the cpp ones its the same
+auth_manager = SpotifyClientCredentials(keys.CLIENT_ID,
+                                        keys.CLIENT_SECRET)  # is in the .gitignore, ask kevin for details or take the cpp ones its the same
 sp = spotipy.Spotify(auth_manager=auth_manager)
+
+
 def findAlbumsFromArtist(artist_uri):
     albums = []
     response_single = sp.artist_albums(artist_uri, album_type='single')
@@ -35,14 +41,14 @@ def findAlbumsFromArtist(artist_uri):
 
 def getAlbumTracks(album_uri):
     response = sp.album_tracks(album_uri)
-    collab_artists = {} # a set of tuples containing all the artists that this artist has worked with
+    collab_artists = {}  # a set of tuples containing all the artists that this artist has worked with
     while response:
         for i, song in enumerate(response['items']):
             if len(song['artists']) > 1:
                 for j, artist in enumerate(song['artists']):
-                    songname = song['name']
+                    songname = (song['id'], song['name'])
                     if songname in collab_artists:
-                        collab_artists[songname].extend([(artist['id'],artist['name'])])
+                        collab_artists[songname].extend([(artist['id'], artist['name'])])
                     else:
                         collab_artists[songname] = [(artist['id'], artist['name'])]
 
@@ -54,35 +60,64 @@ def getAlbumTracks(album_uri):
 
 
 def getAllData(seed_artist_uri):
-
+    data = []
     artist_queue = queue.Queue()
     artist_queue.put(seed_artist_uri[15:])
     i = 0
-    while (not artist_queue.empty()) or i > 20:
+    artist_set = set()
+    while (not artist_queue.empty()) and i < 10:
+        print(i)
         artist_uri = artist_queue.get()
-        artist_set = {artist_uri}
+        connected_artist_set = set()
+        artist_set.add(artist_uri)
         print('spotify:artist:' + artist_uri)
         albums = findAlbumsFromArtist('spotify:artist:' + artist_uri)
         collab_albums = [uri[0] for uri in albums]
         for album_uri in collab_albums:
+
             collab_songs = getAlbumTracks(album_uri)
-            pp.pprint(collab_songs)
+
             for s in collab_songs:
-                #the problem is there will be a time where we have already visited the artist and then we'll do a loopback and basically do Jacob again
-                #if we just use like a disjoint set, then it'll look like a spider and there wont be as many interconnections between artists
-                #but idk how to get the other artists without backing up and redoing
-                #maybe instead we use a disjoint set of songs?
                 songartists = collab_songs[s]
+                # pp.pprint(collab_songs) #printing all the songs
+                comb = combinations(songartists, 2)  # get combinations of the artists that worked on this collab_song
+                for c in list(comb):
+                    # process entry into a list thats [id1, artist1, id2, artist2, id_song, songname]
+                    entry = []
+                    for com in c:
+                        # adds the two artists id and names together to become one list
+                        entry.extend(list(com))
+                    # adds the song id and name to the list
+                    entry.extend(list(s))
+                    data.append(entry)
+
                 uris = [uri[0] for uri in songartists]
                 for uri in uris:
-                    artist_queue.put(uri)
+                    if (uri not in artist_set) and (uri not in connected_artist_set):
+                        connected_artist_set.add(uri)
+                        # print("inserting: ")
+                        # print(sp.artist(uri)["name"])
+                        artist_queue.put(uri)
 
         i += 1
+    return data
 
 
 jacob_collier_uri = 'spotify:artist:0QWrMNukfcVOmgEU0FEDyD'
-getAllData(jacob_collier_uri)
+all_data = getAllData(jacob_collier_uri)
+# data is structured like this:
+# entry
+#   tuple
+#     tuple of first artist and their id
+#     tuple of second artist and their id
+#   tuple of song and its id
+pp.pprint(all_data)
+fields = ["Id1", "Name1", "Id2", "Name2", "IdSong", "NameSong"]
+with open('artist_connections.csv', 'w') as f:
+    # using csv.writer method from CSV package
+    write = csv.writer(f, delimiter=',', quotechar="'",)
 
-
-
-
+    write.writerow(fields)
+    for data_row in all_data:
+        #needs to fix the character encoding for spanish characters
+        write.writerow(data_row)
