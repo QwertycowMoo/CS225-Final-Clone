211827ae5d9ff89912942983bb177625c4977804
evanmm3 <evanmm3@illinois.edu>
2020-12-04 15:11:26 -0600

    bfs/landmark

--- graphArtists/graph.h
+++ graphArtists/graph.h
@@ -1,7 +1,12 @@
 #include "edge.h"
 #include "vertex.h"
+#include <vector>
+#include <queue>
 #pragma once
 
 class Graph {
-    
+    public:
+    void BFS();
+    void landMark();
+    private:
 };
